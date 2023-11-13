70905d1b5731b328cb42ef5ed6275d24899e0538
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2023-11-13 22:21:45 +0200

    Update coeff_detector.py

--- third_part/face3d/coeff_detector.py
+++ third_part/face3d/coeff_detector.py
@@ -1,99 +1,142 @@
 import os
 import glob
 import numpy as np
-from os import makedirs, name
+from os import makedirs
 from PIL import Image
 from tqdm import tqdm
-
 import torch
 import torch.nn as nn
-
 from face3d.options.inference_options import InferenceOptions
 from face3d.models import create_model
 from face3d.util.preprocess import align_img
 from face3d.util.load_mats import load_lm3d
 from face3d.extract_kp_videos import KeypointExtractor
 
+# Pritaikyti SOLID principai: SRP (Single Responsibility Principle) ir OCP (Open/Closed Principle)
 
-class CoeffDetector(nn.Module):
+class IModelHandler:
+    def setup(self, opt):
+        raise NotImplementedError
+
+    def parallelize(self):
+        raise NotImplementedError
+
+    def eval(self):
+        raise NotImplementedError
+
+    def set_input(self, data_input):
+        raise NotImplementedError
+
+    def test(self):
+        raise NotImplementedError
+
+    @property
+    def pred_coeffs_dict(self):
+        raise NotImplementedError
+
+class ModelHandler(IModelHandler):
     def __init__(self, opt):
-        super().__init__()
-
         self.model = create_model(opt)
         self.model.setup(opt)
         self.model.device = 'cuda'
         self.model.parallelize()
         self.model.eval()
 
-        self.lm3d_std = load_lm3d(opt.bfm_folder) 
+    def setup(self, opt):
+        pass
+
+    def parallelize(self):
+        pass
+
+    def eval(self):
+        pass
+
+    def set_input(self, data_input):
+        self.model.set_input(data_input)
+
+    def test(self):
+        self.model.test()
+
+    @property
+    def pred_coeffs_dict(self):
+        return {
+            key: self.model.pred_coeffs_dict[key].cpu().numpy() for key in self.model.pred_coeffs_dict
+        }
+
+class ImageTransformer:
+    def __init__(self, lm3d_std):
+        self.lm3d_std = lm3d_std
+
+    def transform_image(self, images, lm):
+        W, H = images.size
+        if np.mean(lm) == -1:
+            lm = (self.lm3d_std[:, :2] + 1) / 2.
+            lm = np.concatenate([lm[:, :1] * W, lm[:, 1:2] * H], 1)
+        else:
+            lm[:, -1] = H - 1 - lm[:, -1]
+
+        trans_params, img, lm, _ = align_img(images, lm, self.lm3d_std)
+        img = torch.tensor(np.array(img) / 255., dtype=torch.float32).permute(2, 0, 1)
+        trans_params = np.array([float(item) for item in np.hsplit(trans_params, 5)])
+        trans_params = torch.tensor(trans_params.astype(np.float32))
+        return img, trans_params
+
+class CoeffDetector(nn.Module):
+    def __init__(self, model_handler: IModelHandler, image_transformer: ImageTransformer):
+        super().__init__()
+        self.model_handler = model_handler
+        self.image_transformer = image_transformer
 
     def forward(self, img, lm):
-        
-        img, trans_params = self.image_transform(img, lm)
+        img, trans_params = self.image_transformer.transform_image(img, lm)
+        data_input = {
+            'imgs': img[None],
+        }
+        self.model_handler.set_input(data_input)
+        self.model_handler.test()
 
-        data_input = {                
-                'imgs': img[None],
-                }        
-        self.model.set_input(data_input)  
-        self.model.test()
-        pred_coeff = {key:self.model.pred_coeffs_dict[key].cpu().numpy() for key in self.model.pred_coeffs_dict}
+        pred_coeff = {key: self.model_handler.pred_coeffs_dict[key] for key in self.model_handler.pred_coeffs_dict}
         pred_coeff = np.concatenate([
-            pred_coeff['id'], 
-            pred_coeff['exp'], 
-            pred_coeff['tex'], 
+            pred_coeff['id'],
+            pred_coeff['exp'],
+            pred_coeff['tex'],
             pred_coeff['angle'],
             pred_coeff['gamma'],
             pred_coeff['trans'],
             trans_params[None],
-            ], 1)
-        
-        return {'coeff_3dmm':pred_coeff, 
-                'crop_img': Image.fromarray((img.cpu().permute(1, 2, 0).numpy()*255).astype(np.uint8))}
+        ], 1)
 
-    def image_transform(self, images, lm):
-        """
-        param:
-            images:          -- PIL image 
-            lm:              -- numpy array
-        """
-        W,H = images.size
-        if np.mean(lm) == -1:
-            lm = (self.lm3d_std[:, :2]+1)/2.
-            lm = np.concatenate(
-                [lm[:, :1]*W, lm[:, 1:2]*H], 1
-            )
-        else:
-            lm[:, -1] = H - 1 - lm[:, -1]
-
-        trans_params, img, lm, _ = align_img(images, lm, self.lm3d_std)        
-        img = torch.tensor(np.array(img)/255., dtype=torch.float32).permute(2, 0, 1)
-        trans_params = np.array([float(item) for item in np.hsplit(trans_params, 5)])
-        trans_params = torch.tensor(trans_params.astype(np.float32))
-        return img, trans_params        
+        return {
+            'coeff_3dmm': pred_coeff,
+            'crop_img': Image.fromarray((img.cpu().permute(1, 2, 0).numpy() * 255).astype(np.uint8))
+        }
 
 def get_data_path(root, keypoint_root):
     filenames = list()
     keypoint_filenames = list()
-
     IMAGE_EXTENSIONS_LOWERCASE = {'jpg', 'png', 'jpeg', 'webp'}
     IMAGE_EXTENSIONS = IMAGE_EXTENSIONS_LOWERCASE.union({f.upper() for f in IMAGE_EXTENSIONS_LOWERCASE})
     extensions = IMAGE_EXTENSIONS
 
     for ext in extensions:
         filenames += glob.glob(f'{root}/*.{ext}', recursive=True)
+
     filenames = sorted(filenames)
     for filename in filenames:
         name = os.path.splitext(os.path.basename(filename))[0]
         keypoint_filenames.append(
             os.path.join(keypoint_root, name + '.txt')
         )
+
     return filenames, keypoint_filenames
 
-
 if __name__ == "__main__":
-    opt = InferenceOptions().parse() 
-    coeff_detector = CoeffDetector(opt)
+    opt = InferenceOptions().parse()
+    model_handler = ModelHandler(opt)
+    image_transformer = ImageTransformer(load_lm3d(opt.bfm_folder))
+    coeff_detector = CoeffDetector(model_handler, image_transformer)
     kp_extractor = KeypointExtractor()
+
     image_names, keypoint_names = get_data_path(opt.input_dir, opt.keypoint_dir)
     makedirs(opt.keypoint_dir, exist_ok=True)
     makedirs(opt.output_dir, exist_ok=True)
@@ -104,15 +147,109 @@ if __name__ == "__main__":
             lm = kp_extractor.extract_keypoint(image, keypoint_name)
         else:
             lm = np.loadtxt(keypoint_name).astype(np.float32)
-            lm = lm.reshape([-1, 2]) 
+            lm = lm.reshape([-1, 2])
         predicted = coeff_detector(image, lm)
         name = os.path.splitext(os.path.basename(image_name))[0]
         np.savetxt(
-            "{}/{}_3dmm_coeff.txt".format(opt.output_dir, name), 
-            predicted['coeff_3dmm'].reshape(-1))
+            "{}/{}_3dmm_coeff.txt".format(opt.output_dir, name),
+            predicted['coeff_3dmm'].reshape(-1)
+        )
 
+
+
+# import os
+# import glob
+# import numpy as np
+# from os import makedirs, name
+# from PIL import Image
+# from tqdm import tqdm
+# import torch
+# import torch.nn as nn
+# from face3d.options.inference_options import InferenceOptions
+# from face3d.models import create_model
+# from face3d.util.preprocess import align_img
+# from face3d.util.load_mats import load_lm3d
+# from face3d.extract_kp_videos import KeypointExtractor
+# class CoeffDetector(nn.Module):
+#     def __init__(self, opt):
+#         super().__init__()
+#         self.model = create_model(opt)
+#         self.model.setup(opt)
+#         self.model.device = 'cuda'
+#         self.model.parallelize()
+#         self.model.eval()
+#         self.lm3d_std = load_lm3d(opt.bfm_folder) 
+#     def forward(self, img, lm):
         
-
-
-
-    
+#         img, trans_params = self.image_transform(img, lm)
+#         data_input = {                
+#                 'imgs': img[None],
+#                 }        
+#         self.model.set_input(data_input)  
+#         self.model.test()
+#         pred_coeff = {key:self.model.pred_coeffs_dict[key].cpu().numpy() for key in self.model.pred_coeffs_dict}
+#         pred_coeff = np.concatenate([
+#             pred_coeff['id'], 
+#             pred_coeff['exp'], 
+#             pred_coeff['tex'], 
+#             pred_coeff['angle'],
+#             pred_coeff['gamma'],
+#             pred_coeff['trans'],
+#             trans_params[None],
+#             ], 1)
+        
+#         return {'coeff_3dmm':pred_coeff, 
+#                 'crop_img': Image.fromarray((img.cpu().permute(1, 2, 0).numpy()*255).astype(np.uint8))}
+#     def image_transform(self, images, lm):
+#         """
+#         param:
+#             images:          -- PIL image 
+#             lm:              -- numpy array
+#         """
+#         W,H = images.size
+#         if np.mean(lm) == -1:
+#             lm = (self.lm3d_std[:, :2]+1)/2.
+#             lm = np.concatenate(
+#                 [lm[:, :1]*W, lm[:, 1:2]*H], 1
+#             )
+#         else:
+#             lm[:, -1] = H - 1 - lm[:, -1]
+#         trans_params, img, lm, _ = align_img(images, lm, self.lm3d_std)        
+#         img = torch.tensor(np.array(img)/255., dtype=torch.float32).permute(2, 0, 1)
+#         trans_params = np.array([float(item) for item in np.hsplit(trans_params, 5)])
+#         trans_params = torch.tensor(trans_params.astype(np.float32))
+#         return img, trans_params        
+# def get_data_path(root, keypoint_root):
+#     filenames = list()
+#     keypoint_filenames = list()
+#     IMAGE_EXTENSIONS_LOWERCASE = {'jpg', 'png', 'jpeg', 'webp'}
+#     IMAGE_EXTENSIONS = IMAGE_EXTENSIONS_LOWERCASE.union({f.upper() for f in IMAGE_EXTENSIONS_LOWERCASE})
+#     extensions = IMAGE_EXTENSIONS
+#     for ext in extensions:
+#         filenames += glob.glob(f'{root}/*.{ext}', recursive=True)
+#     filenames = sorted(filenames)
+#     for filename in filenames:
+#         name = os.path.splitext(os.path.basename(filename))[0]
+#         keypoint_filenames.append(
+#             os.path.join(keypoint_root, name + '.txt')
+#         )
+#     return filenames, keypoint_filenames
+# if __name__ == "__main__":
+#     opt = InferenceOptions().parse() 
+#     coeff_detector = CoeffDetector(opt)
+#     kp_extractor = KeypointExtractor()
+#     image_names, keypoint_names = get_data_path(opt.input_dir, opt.keypoint_dir)
+#     makedirs(opt.keypoint_dir, exist_ok=True)
+#     makedirs(opt.output_dir, exist_ok=True)
+#     for image_name, keypoint_name in tqdm(zip(image_names, keypoint_names)):
+#         image = Image.open(image_name)
+#         if not os.path.isfile(keypoint_name):
+#             lm = kp_extractor.extract_keypoint(image, keypoint_name)
+#         else:
+#             lm = np.loadtxt(keypoint_name).astype(np.float32)
+#             lm = lm.reshape([-1, 2]) 
+#         predicted = coeff_detector(image, lm)
+#         name = os.path.splitext(os.path.basename(image_name))[0]
+#         np.savetxt(
+#             "{}/{}_3dmm_coeff.txt".format(opt.output_dir, name), 
+#             predicted['coeff_3dmm'].reshape(-1))
